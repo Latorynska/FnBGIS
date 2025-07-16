649bf612861505c01868d5250251fafad7d2e515
latorynska <paksiringkang225@gmail.com>
2025-07-16 11:20:11 +0700

    update : loadingscreen on guestroute

--- src/routes/GuestRoute.jsx
+++ src/routes/GuestRoute.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { loadUserData } from '../redux/thunks/authApi';
 import { useEffect, useState } from 'react';
+import LoadingScreen from '../pages/LoadingScreen/LoadingScreen';
 
 const GuestRoute = () => {
   const dispatch = useDispatch();
@@ -10,24 +11,33 @@ const GuestRoute = () => {
   const loading = useSelector((state) => state.auth.loading);
   const userData = useSelector((state) => state.auth.userData);
 
+  const [showLoading, setShowLoading] = useState(true);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        if (auth.currentUser !== null && !userData) {
-          dispatch(loadUserData(auth.currentUser.uid));
-        }
+      if (user && auth.currentUser && !userData) {
+        dispatch(loadUserData(auth.currentUser.uid));
       }
     });
 
     return () => unsubscribe();
   }, [auth, dispatch, userData]);
 
-  if (loading) {
-    return <div>Loading...</div>;
+  useEffect(() => {
+    if (!loading) {
+      const timeout = setTimeout(() => {
+        setShowLoading(false);
+      }, 400); // waktu tunggu animasi keluar
+      return () => clearTimeout(timeout);
+    }
+  }, [loading]);
+
+  if (showLoading) {
+    return <LoadingScreen isVisible={showLoading} />;
   }
 
   if (auth.currentUser) {
-    return <Navigate to={'/Dashboard'} />;
+    return <Navigate to="/dashboard" />;
   }
 
   return <Outlet />;
